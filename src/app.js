a424dc2ed5e58a83985be8a4e2a93b5af4315697
zharfan104 <zharfan.akbar104@gmail.com>
2020-09-22 18:08:23 +0700

    oik

--- src/app.js
+++ src/app.js
@@ -8,13 +8,39 @@ const methodOverride = require("method-override");
 const initializePassport = require("../src/routes/passport-config");
 const app = express();
 var expressLayouts = require('express-ejs-layouts');
+// const minify = require('@node-minify/core');
+// const clss = require(' @node-minify/clean-css ');
+// const uglifyjs = require('@node-minify/uglify-js');
+// const htmlmin = require('@node-minify/html-minifier');
+
+// minify({
+//     compressor: clss,
+//     input: './**/*.css',
+//     output: './**/*.js',
+//     callback: function (err, min) {}
+// });
+
+// // Using UglifyJS
+// minify({
+//     compressor: uglifyjs,
+//     input: './**/*.js',
+//     output: './**/*.js',
+//     callback: function (err, min) {}
+// });
+
+// // Using Promise
+// var promise = minify({
+//     compressor: htmlmin,
+//     input: './**/*.js',
+//     output: './**/*.js'
+// });
 
 // Settings
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.set('layout extractScripts', true)
+app.set('layout extractScripts', true);
 app.set('layout extractStyles', true)
 
 app.use(expressLayouts);
